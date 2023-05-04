77e2e263278666edd37dc7a19379cab98f395c36
rubengardner <rgardnercuesta@gmail.com>
2023-05-04 12:35:20 +0000

    Add urls properly with comma

--- exercises/urls.py
+++ exercises/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path('exercises/', views.ExerciseList.as_view())
+    path('exercises/', views.ExerciseList.as_view()),
     path('exercises/<int:pk>', views.ExerciseSpecific.as_view())
 ]
