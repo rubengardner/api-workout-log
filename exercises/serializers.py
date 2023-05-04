954c11be83368a2ebb63a0f111b7ef3d8823fd8b
rubengardner <rgardnercuesta@gmail.com>
2023-05-04 11:57:07 +0000

    Add exercise views and serializers

---
+++ exercises/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Exercise
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Exercise
+        fields = [
+            'id', 'owner', 'profile_id', 'created_at',
+            'updated_at', 'name', 'is_owner', 
+            'unit_1','unit_2',
+        ]
--- exercises/views.py
+++ exercises/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from drf_workoutlog.permissions import IsOwnerOrReadOnly
+from .models import Exercise
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import ExerciseSerializer
 
-# Create your views here.
+class ExerciseList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
+    serializer_class = ExerciseSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Exercise.objects.all().order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = []
+    search_fields =[]
+    ordering_fields = []
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+
+class ExerciseSpecific(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Exercise.objects.all().order_by('-created_at')
