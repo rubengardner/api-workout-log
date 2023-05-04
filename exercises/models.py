18c921b5ad97eff239902b44a52fa5eaaef42a42
rubengardner <rgardnercuesta@gmail.com>
2023-05-04 10:45:16 +0000

    Add exercises models

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Exercise(models.Model):
+    unit_choices = [
+        ('kg', 'kg'), ('sec', 'sec'),
+        ('m', 'm')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, unique=True)
+    
+    unit_1 = models.CharField(max_length=40, choices=unit_choices, default='kg')
+    unit_2 = models.CharField(max_length=40, choices=unit_choices, default= None)
+   
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.name}'
