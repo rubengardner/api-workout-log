c5fbb47b19993823480d7cafd72e653bb3a3063f
rubengardner <rgardnercuesta@gmail.com>
2023-05-04 11:50:38 +0000

    Add functionality to profile app

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,28 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status, filters, generics
+from rest_framework.views import APIView
+from django.http import Http404
 from .models import Profile
+from django.db.models import Count
 from .serializers import ProfileSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
-class ProfileList(APIView):
-    """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
-    """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all().order_by('-created_at')
+    serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+    ]
+    ordering_fields = [
+    ]
+
+
+class ProfileSpecific(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all().order_by('-created_at')
