cb1db4deeedc8df5ea2e23074caaf95fe1a8e85f
rubengardner <rgardnercuesta@gmail.com>
2023-05-04 12:01:24 +0000

    Change drf_workoutlog naming bug

--- exercises/views.py
+++ exercises/views.py
@@ -2,7 +2,6 @@ from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from drf_workoutlog.permissions import IsOwnerOrReadOnly
 from .models import Exercise
-from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ExerciseSerializer
 
 class ExerciseList(generics.ListCreateAPIView):
@@ -13,14 +12,7 @@ class ExerciseList(generics.ListCreateAPIView):
     serializer_class = ExerciseSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Exercise.objects.all().order_by('-created_at')
-    filter_backends = [
-        filters.OrderingFilter,
-        filters.SearchFilter,
-        DjangoFilterBackend,
-    ]
-    filterset_fields = []
-    search_fields =[]
-    ordering_fields = []
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- profiles/views.py
+++ profiles/views.py
@@ -5,21 +5,13 @@ from django.http import Http404
 from .models import Profile
 from django.db.models import Count
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
+from drf_workoutlog.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
     queryset = Profile.objects.all().order_by('-created_at')
     serializer_class = ProfileSerializer
-    filter_backends = [
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filterset_fields = [
-    ]
-    ordering_fields = [
-    ]
+
 
 
 class ProfileSpecific(generics.RetrieveUpdateAPIView):
