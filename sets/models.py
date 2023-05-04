9a7f98cae82bb4cbcc93b81f02d63e63ad3d2ebe
rubengardner <rgardnercuesta@gmail.com>
2023-05-04 13:18:20 +0000

    Add sets app and corresponding model

---
+++ sets/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from exercises.models import Exercise
+from workouts.models import Workout
+from django.contrib.auth.models import User
+
+
+class Set(models.Model):
+
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    workout = models.ForeignKey(workout, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reps = models.PositiveIntegerField()
+    value_of_unit_1 = models.IntegerField()
+    value_of_unit_2 = models.IntegerField(default= None, blank=True)
+   
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Set of: {self.exercise} '
