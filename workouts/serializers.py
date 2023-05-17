7c547e73743647f9001af95c5b9c47ac3109b3b0
rubengardner <rgardnercuesta@gmail.com>
2023-05-17 18:20:32 +0000

    Modify serializer in Workout

--- workouts/serializers.py
+++ workouts/serializers.py
@@ -6,9 +6,10 @@ class WorkoutSerializer(serializers.ModelSerializer):
     sets_count = serializers.ReadOnlyField()
     owner = serializers.ReadOnlyField(source='owner.username')
     exercise_count = serializers.ReadOnlyField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     
     class Meta:
         model = Workout
         fields = [
-            'id', 'created_at', 'sets_count', 'owner', 'exercise_count'
+            'id', 'created_at', 'sets_count', 'owner', 'exercise_count', 'profile_id'
         ]
