74bf578618f3ba54675f439ef4c2412f520c05b2
rubengardner <rgardnercuesta@gmail.com>
2023-05-22 19:50:24 +0000

    Add is_owner field to workouts

--- workouts/serializers.py
+++ workouts/serializers.py
@@ -7,9 +7,15 @@ class WorkoutSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     exercise_count = serializers.ReadOnlyField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
     
     class Meta:
         model = Workout
         fields = [
-            'id', 'created_at', 'sets_count', 'owner', 'exercise_count', 'profile_id', 'date'
+            'id', 'created_at', 'sets_count', 'owner', 'exercise_count', 'profile_id', 'date',
+            'is_owner'
         ]
